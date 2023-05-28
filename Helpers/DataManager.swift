76a27078a2ea79143de70e7e7ac58caf441e4643
Benjamin Groom <99204914+Ben-KCL@users.noreply.github.com>
2023-05-28 22:32:16 +0100

    Create functions to retrieve StopPoint data from the TfL API or a local cache and parse it.

---
+++ Helpers/DataManager.swift
@@ -0,0 +1,8 @@
+//
+//  DataManager.swift
+//  Tube Picker
+//
+//  Created by Benjamin Groom on 26/05/2023.
+//
+
+import Foundation
---
+++ Model/StopPoint.swift
@@ -0,0 +1,8 @@
+//
+//  StopPoint.swift
+//  Tube Picker
+//
+//  Created by Benjamin Groom on 26/05/2023.
+//
+
+import Foundation
